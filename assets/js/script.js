f436593058fea17e303efc96b6a48d4326839e94
kmelter <kmelter97@gmail.com>
2022-03-18 17:13:17 -0500

    insert state dropdown

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,28 @@
-var cityName = "placeholder"; //city name input from user
+var cityName = document.getElementById("citysearch").textContent; //city name input from user
 var stateCode = "placeholder"; //maybe make a dropdown of states
 
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function stateSelect() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+}
+
 function getCoordinates() {
-    var requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + ",{state code},{country code}&limit={limit}&appid={API key}"
+    var requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + ",{state code},US&units=imperial&appid={API key}"
 }
 
 //TODO:
