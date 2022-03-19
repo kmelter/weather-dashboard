aa0bae01db0cadf7566245b78df1a2ca3c98c40e
kmelter <kmelter97@gmail.com>
2022-03-19 11:49:09 -0500

    successful fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
-var cityName = document.getElementById("citysearch");
-var stateCode = document.getElementById("stateselect").value;
+var userFormEl = document.querySelector("#user-form");
+var cityInput = document.querySelector("#citysearch");
+var stateSelect = document.querySelector("#stateselect");
 
-console.log(cityName);
-console.log(stateCode);
+
 
 
 var getWeatherData = function(/*pass in coordinates*/) {
-    var requestUrl = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}";
+    var requestUrl = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&units=imperial&appid={API key}";
 
     fetch(requestUrl).then(function(response) {
         if (response.ok) {
@@ -22,16 +22,18 @@ var getWeatherData = function(/*pass in coordinates*/) {
 
 var getCoordinates = function(cityName, stateCode) {
     //format openweather url
-    var requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "," + stateCode + ",US&units=imperial&appid={API key}"
+    var requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "," + stateCode + ",US&limit=1&appid=12aa7dcc32e49f0342370d0c3b3204d7";
     
+
     //make a request to the url
     fetch(requestUrl).then(function(response) {
         if (response.ok) {
-            response.json().then(function(data) { //these two lines might not be right
-                getWeatherData(/*pass in lat and lon coordinates*/);
+            response.json().then(function(data) {
+                console.log(data);
+                //getWeatherData(/*pass in data to reference lat and lon coordinates*/);
             });
         } else {
-            alert("Error: Please enter a city");
+            alert("Error: Please enter valid location");
         }
     })
     .catch(function(error) {
@@ -39,11 +41,22 @@ var getCoordinates = function(cityName, stateCode) {
     });
 };
 
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    var cityName = cityInput.value.trim();
+    var stateCode = stateSelect.value.trim();
 
-$("#submitbutton").on("click", function() {
-    getCoordinates();
+    if (cityName, stateCode) {
+        getCoordinates(cityName, stateCode);
+        cityInput.value = "";
+    } else {
+        alert("Please enter city and state");
+    }
+};
 
-});
+
+userFormEl.addEventListener("submit", formSubmitHandler);
+
 //TODO:
 // * call api to find coordinates from city name and state code inputs
 // * call api to find weather data and change temperature units to fahrenheit in url
