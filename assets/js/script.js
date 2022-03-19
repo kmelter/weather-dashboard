8c1ed2ad58fba01c53968097851311e4c73ed2a4
kmelter <kmelter97@gmail.com>
2022-03-19 13:54:20 -0500

    fetch current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,19 +3,34 @@ var cityInput = document.querySelector("#citysearch");
 var stateSelect = document.querySelector("#stateselect");
 
 
+var insertData = function(data) {
+    var currentTemp = data.current.temp.toString();
+    var currentWind = data.current.wind_speed.toString();
+    var currentHumidity = data.current.humidity.toString();
+    var currentUV = data.current.uvi.toString();
 
+    
+    document.getElementById("selected-city").textContent = cityInput.value.trim();
+    document.getElementById("current-temp").textContent = currentTemp;
+    document.getElementById("current-wind").textContent = currentWind;
+    document.getElementById("current-humidity").textContent = currentHumidity;
+    document.getElementById("current-uv").textContent = currentUV;
+};
 
 var getWeatherData = function(data) {
-    var latitude = data.lat.json.stringify();  // these two lines
-    var longitude = data.lon.json.stringify(); // are not correct
+    var latitude = data[0].lat.toString();  // these two lines
+    var longitude = data[0].lon.toString(); // are not correct
     
-    var requestUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude={part}&units=imperial&appid=12aa7dcc32e49f0342370d0c3b3204d7";
+    console.log(latitude);
+    console.log(longitude);
+
+    var requestUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&units=imperial&appid=12aa7dcc32e49f0342370d0c3b3204d7";
 
     fetch(requestUrl).then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
                 console.log(data);
-                //call function to insert data into correct elements
+                insertData(data);
             });
         } 
     })
@@ -28,6 +43,8 @@ var getCoordinates = function(cityName, stateCode) {
     //format openweather url
     var requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "," + stateCode + ",US&limit=1&appid=12aa7dcc32e49f0342370d0c3b3204d7";
     
+    console.log(cityName);
+    console.log(stateCode);
 
     //make a request to the url
     fetch(requestUrl).then(function(response) {
@@ -50,9 +67,11 @@ var formSubmitHandler = function(event) {
     var cityName = cityInput.value.trim();
     var stateCode = stateSelect.value.trim();
 
+    //TODO: store search in local storage
+
     if (cityName, stateCode) {
         getCoordinates(cityName, stateCode);
-        cityInput.value = "";
+        // cityInput.value = "";
     } else {
         alert("Please enter city and state");
     }
